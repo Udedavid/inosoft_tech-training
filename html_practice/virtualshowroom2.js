0db42e76320bfa3429ea209fa9c9a5e66af4507b
Udedavid <you@example.com>
2023-11-10 11:59:19 +0100

    google

---
+++ html_practice/virtualshowroom2.js
@@ -0,0 +1,11 @@
+const navbar = document.getElementById("navbar");
+
+window.addEventListener('scroll', () => {
+    const scrollPosition = window.scrollY;
+
+    if (scrollPosition > 1) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
